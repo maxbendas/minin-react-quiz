a42af5d34d686fe986126472de8ae3efa779f651
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-04-18 21:54:34 +0300

    9.6

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,135 @@
-import React from 'react';
+import React, {Component} from 'react';
+import classes from './Auth.module.scss';
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
+import is from 'is_js'
 
-const Auth = () => {
-    return (
-        <div>
-            Auth
-        </div>
-    );
-};
+// function validateEmail(email) {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+class Auth extends Component {
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Enter the correct email',
+                valid: 'false',
+                touched: 'false',
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Enter the correct password',
+                valid: 'false',
+                touched: 'false',
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+    loginHandler = () => {
+        console.log(11)
+    }
+    registerHandler = () => {
+
+    }
+    submitHandler = (e) => {
+        e.preventDefault()
+    }
+
+
+    validateControl = (value, validation) => {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+        return isValid
+
+    }
+
+    onChangeHandler = (e, controlName) => {
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
+
+        control.value = e.target.value
+        control.tuched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            return isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
+        })
+    }
+
+    renderInputs = () => {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    shouldValidate={!!control.validation}
+                    {...control}
+                    onChange={(e) => this.onChangeHandler(e, controlName)}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+                    <form onSubmit={this.submitHandler}>
+                        {this.renderInputs()}
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Sign In
+                        </Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Sign Up
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Auth;
--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import classes from './QuizCreator.module.scss'
+import Button from "../../components/UI/Button/Button";
 
-const QuizCreator = () => {
-    return (
-        <div>
-            QuizCreator
-        </div>
-    );
-};
+class QuizCreator extends Component {
+    submitHandler = (e) => {
+        e.preventDefault()
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    createQuizHandler = ()=>{
+
+    }
+
+    render() {
+        return (
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Create quiz</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <input type="text"/>
+                        <hr/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <select></select>
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                        >
+                            Add question
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                        >
+                            Create quiz
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default QuizCreator;
