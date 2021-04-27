c8755a8f392bd53cd24a3301504758ede2f6dd89
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-04-27 21:51:12 +0300

    final server

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.scss';
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from 'is_js'
+import axios from "axios";
 
 // function validateEmail(email) {
 //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -40,11 +41,31 @@ class Auth extends Component {
             }
         }
     }
-    loginHandler = () => {
-        console.log(11)
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken:true
+        }
+        try{
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDCXhWGHgoyPaS--SSPciIvAhHmFNsBi9w', authData)
+            console.log(response.data)
+        }catch(e){
+            console.log(e)
+        }
     }
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken:true
+        }
+        try{
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCXhWGHgoyPaS--SSPciIvAhHmFNsBi9w', authData)
+            console.log(response.data)
+        }catch(e){
+            console.log(e)
+        }
     }
     submitHandler = (e) => {
         e.preventDefault()
